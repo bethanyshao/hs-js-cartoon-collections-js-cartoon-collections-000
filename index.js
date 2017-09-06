60e58b68da2e9f8c113d82735252e12a7b3b2ed7
Bethany Shao <bethany.magdalena@gmail.com>
2017-09-06 18:21:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 function dwarfRollCall(dwarves) {
+  var dwarfString = "";
+  var num = 1;
+  dwarves.forEach(function(dwarf) {
+    dwarfString += num + ". " + dwarf + " ";
+    num++;
+  });
+  return dwarfString;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  return planeteerCalls.map(function(call) {
+    return call.toUpperCase() + "!";
+  });
 }
 
 function longPlaneteerCalls(words) {
+  var longerThan4 = false;
+  words.forEach(function(word) {
+    if(word.length > 4) {
+      longerThan4 = true;
+    }
+  });
+  return longerThan4;
 }
 
 function findTheCheese (foods) {
+  // cheeses: cheddar, gouda, and camembert
+  var containsCheese = false;
+  var firstCheese = "";
+  foods.forEach(function(food) {
+    if(food === "cheddar" || food === "gouda" || food === "camembert") {
+      containsCheese = true;
+      firstCheese = food;
+    }
+  });
+  if(containsCheese) {
+    return firstCheese;
+  } else {
+    return "no cheese!";
+  }
 }
