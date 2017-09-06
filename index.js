9841b9abce08d06377f908c1825ca196f99760cb
Bethany Shao <bethany.magdalena@gmail.com>
2017-09-06 17:45:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function dwarfRollCall(dwarves) {
+  var dwarfString = "";
+  var num = 1;
+  for(var i = 0; i < dwarves.length; i++) {
+
+  }
+  return dwarfString.trim();
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  
 }
 
 function longPlaneteerCalls(words) {
@@ -9,3 +16,5 @@ function longPlaneteerCalls(words) {
 
 function findTheCheese (foods) {
 }
+
+summonCaptainPlanet(["earth", "wind", ])
